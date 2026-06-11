2fcf482ce30005bef7d476cab4b693071b0ca192
Jai Chaubey <vanced.exe@gmail.com>
2026-06-11 21:14:11 +0530

    created weather based app understood api and aysnc practical

---
+++ 02_weather_starter/02_weather/script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const cityInput = document.getElementById("city-input");
+    const getWeatherBtn = document.getElementById("get-weather-btn");
+    const  weatherInfo = document.getElementById("weather-info");
+    const cityNameDisplay = document.getElementById("city-name");
+    const tempDisplay = document.getElementById("temperature");
+    const descriptionDisplay = document.getElementById("description");
+    const errorMessage = document.getElementById("error-message");
+
+    const API_KEY = "47bd10891ab23a76281fe522543b04ea"; //env variable
+
+     getWeatherBtn.addEventListener('click',async ()=> {
+        const city = cityInput.value.trim()
+        if(!city) return;
+
+     //error and takes time
+
+     try {
+       weatherData = await fetchWeatherData(city)
+       displayWeatherData(weatherData)
+     } catch (error) {
+        showError()
+     }
+
+     })
+     async function fetchWeatherData(city){
+        //gets the data
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+        const response = await fetch(url);
+        console.log(typeof response);
+        console.log("respone",response);
+        
+    if(!response.ok){
+        throw new Error("City Not Found");
+    }
+        const data = await response.json()
+        return data
+    
+     }
+     function displayWeatherData(data){
+        console.log(data);
+        const{name, main, weather} = data
+        cityNameDisplay.textContent = name; 
+         tempDisplay.textContent = `Temprature : ${main.temp}`;
+         descriptionDisplay.textContent = `Weather :${weather[0].description}`;
+        //unhide 
+        weatherInfo.classList.remove('hidden')
+        errorMessage.classList.add('hidden')
+       
+     }
+    function showError(weatherData) {
+        weatherInfo.classList.remove('hidden');
+        errorMessage.classList.add('hidden')
+       }
+})
